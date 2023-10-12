eaa6a9b19c979f58d6ab7bf35ea26f0af1ce1119
Hamid Achaou <hamidAchaou379@gmail.com>
2023-10-12 02:54:45 -0700

    addmaquett niveau management

---
+++ Maqute-Gestion-Niveaux/index.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- Bootstrap 5 CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <!-- Font Awesome CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <!-- AdminLTE CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.2.0/dist/css/adminlte.min.css">
+</head>
+<body class="sidebar-mini">
+    <div class="wrapper">
+        <!-- Navbar -->
+        <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+            <!-- Left Navbar Links -->
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
+                </li>
+            </ul>
+        </nav>
+        <!-- Main Sidebar Container -->
+        <?php include_once "./layouts/Sidebar.php" ?>
+        <!-- Content Wrapper -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <div class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1 class="m-0">Skills</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Main content -->
+            <section class="content">
+                <div class="container-fluid">
+                    <div class="card">
+                        <div class="card-header">
+                            <h3 class="card-title">Skills</h3>
+                            <div class="card-tools">
+                                <a href="./ajouter-competences.php" class="btn btn-primary"><i class="fas fa-plus"></i> Ajouter</a>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <table class="table table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th>Nom</th>
+                                        <th>Description</th>
+                                        <th>Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>emetteur</td>
+                                        <td>HyperText Markup Language</td>
+                                        <td>
+                                            <a href="./edit-competences.php" class="btn btn-xs btn-primary"><i class="fas fa-edit"></i> Edit</a>
+                                            <a href="#" class="btn btn-xs btn-danger"><i class="fas fa-trash"></i> Delete</a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>adapters</td>
+                                        <td>Cascading Style Sheets</td>
+                                        <td>
+                                            <a href="./edit-competences.php" class="btn btn-xs btn-primary"><i class="fas fa-edit"></i> Edit</a>
+                                            <a href="#" class="btn btn-xs btn-danger"><i class="fas fa-trash"></i> Delete</a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>maitrises</td>
+                                        <td>Cascading Style Sheets</td>
+                                        <td>
+                                            <a href="./edit-competences.php" class="btn btn-xs btn-primary"><i class="fas fa-edit"></i> Edit</a>
+                                            <a href="#" class="btn btn-xs btn-danger"><i class="fas fa-trash"></i> Delete</a>
+                                        </td>
+                                    </tr>
+                                    <!-- Add more rows for additional skills -->
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- /.content -->
+        </div>
+        <!-- /.content-wrapper -->
+
+        <!-- jQuery -->
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <!-- Bootstrap 5 JavaScript -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- AdminLTE JavaScript -->
+        <script src="https://cdn.jsdelivr.net/npm/admin-lte@3.2.0/dist/js/adminlte.min.js"></script>
+    </div>
+    <!-- /.wrapper -->
+</body>
+</html>
---
+++ Maqute-Gestion-Niveaux/layouts/Sidebar.php
@@ -0,0 +1,43 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+            <!-- Brand Logo -->
+            <a href="#" class="brand-link">
+                <span class="brand-text font-weight-light">AdminLTE</span>
+            </a>
+            <!-- Sidebar -->
+            <div class="sidebar">
+                <!-- Sidebar Menu -->
+                <nav class="mt-2">
+                    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                        <!-- Skills Management -->
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon fas fa-cogs"></i>
+                                <p>
+                                Gestion des compétences
+                                </p>
+                            </a>
+                        </li>
+                        <!-- Niveau Management -->
+                        <li class="nav-item">
+                            <a href="./index.php" class="nav-link">
+                                <i class="nav-icon fas fa-cog"></i>
+                                <p>
+                                Gestion des niveaux
+                                </p>
+                            </a>
+                        </li>
+                        <!-- Interns Management -->
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon fas fa-users"></i>
+                                <p>
+                                Gestion des stagiaires
+                                </p>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+                <!-- /.sidebar-menu -->
+            </div>
+            <!-- /.sidebar -->
+        </aside>
