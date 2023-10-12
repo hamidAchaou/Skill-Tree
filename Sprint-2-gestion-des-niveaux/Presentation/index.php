32f4587d217c21a92d55434d7b9b6eff1d1d8a1b
Hamid Achaou <hamidAchaou379@gmail.com>
2023-10-12 07:32:05 -0700

    add icon logout

--- Sprint-2-gestion-des-niveaux/Presentation/index.php
+++ Sprint-2-gestion-des-niveaux/Presentation/index.php
@@ -14,32 +14,48 @@ if (!isset($_SESSION['Nom'])) {
 
 <body class="sidebar-mini">
     <div class="wrapper">
-        <!-- Navbar -->
-        <nav class="main-header navbar navbar-expand navbar-white navbar-light position-fixed w-100" style="top: 0;">
-            <!-- Left Navbar Links -->
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
-                </li>
-            </ul>
+<!-- Navbar -->
+<nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" data-widget="pushmenu" href="#" role="button">
+                <i class="fas fa-bars"></i>
+            </a>
+        </li>
+    </ul>
 
-            <!-- Right Navbar Links -->
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="#"><?php echo $_SESSION['Nom']; ?></a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link" data-toggle="dropdown" href="#">
-                        <img src="../Presentation/asset/images/logo.png" class="img-circle elevation-2" alt="User Image" style="width: 30px; height: 30px;">
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-right">
-                        <a href="#" class="dropdown-item"><?php echo $_SESSION['Nom']; ?></a>
-                        <div class="dropdown-divider"></div>
-                        <a href="../BLL/authBLO/logoute.php" class="dropdown-item">Logout</a>
-                    </div>
-                </li>
-            </ul>
-        </nav>
+    <!-- Search Form -->
+    <form class="form-inline ml-3">
+        <div class="input-group input-group-sm">
+            <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+            <div class="input-group-append">
+                <button class="btn btn-navbar" type="submit">
+                    <i class="fas fa-search"></i>
+                </button>
+            </div>
+        </div>
+    </form>
+
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+        <!-- Example of a dropdown menu -->
+        <li class="nav-item dropdown">
+            <a class="nav-link" data-toggle="dropdown" href="#">
+                <i class="fas fa-user"></i>
+                <span class="d-lg-none">User</span>
+            </a>
+            <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+                <span class="dropdown-header">User Menu</span>
+                <div class="dropdown-divider"></div>
+                <a href="#" class="dropdown-item">Profile</a>
+                <a href="#" class="dropdown-item">Settings</a>
+                <div class="dropdown-divider"></div>
+                <a href="#" class="dropdown-item">Logout</a>
+            </div>
+        </li>
+    </ul>
+</nav>
         <!-- Main Sidebar Container -->
         <?php include_once "./layouts/Sidebar.php" ?>
         <!-- Content Wrapper -->
@@ -140,8 +156,8 @@ if (!isset($_SESSION['Nom'])) {
         <!-- jQuery -->
         <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
         <!-- Bootstrap 5 JavaScript -->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-        <!-- AdminLTE JavaScript -->
+        <!-- Bootstrap JavaScript -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>        <!-- AdminLTE JavaScript -->
         <script src="https://cdn.jsdelivr.net/npm/admin-lte@3.2.0/dist/js/adminlte.min.js"></script>
     </div>
     <!-- /.wrapper -->
