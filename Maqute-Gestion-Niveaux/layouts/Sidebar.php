e80bf344128ec1d0d776561f71423911f96ea348
Hamid Achaou <hamidAchaou379@gmail.com>
2023-10-12 03:03:02 -0700

    add maqutt niveaux management

--- Maqute-Gestion-Niveaux/layouts/Sidebar.php
+++ Maqute-Gestion-Niveaux/layouts/Sidebar.php
@@ -1,43 +1,54 @@
-<aside class="main-sidebar sidebar-dark-primary elevation-4">
-            <!-- Brand Logo -->
-            <a href="#" class="brand-link">
-                <span class="brand-text font-weight-light">AdminLTE</span>
-            </a>
-            <!-- Sidebar -->
-            <div class="sidebar">
-                <!-- Sidebar Menu -->
-                <nav class="mt-2">
-                    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-                        <!-- Skills Management -->
-                        <li class="nav-item">
-                            <a href="#" class="nav-link">
-                                <i class="nav-icon fas fa-cogs"></i>
-                                <p>
-                                Gestion des compétences
-                                </p>
-                            </a>
-                        </li>
-                        <!-- Niveau Management -->
-                        <li class="nav-item">
-                            <a href="./index.php" class="nav-link">
-                                <i class="nav-icon fas fa-cog"></i>
-                                <p>
-                                Gestion des niveaux
-                                </p>
-                            </a>
-                        </li>
-                        <!-- Interns Management -->
-                        <li class="nav-item">
-                            <a href="#" class="nav-link">
-                                <i class="nav-icon fas fa-users"></i>
-                                <p>
-                                Gestion des stagiaires
-                                </p>
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
-                <!-- /.sidebar-menu -->
-            </div>
-            <!-- /.sidebar -->
-        </aside>
+<aside class="main-sidebar sidebar-dark-primary elevation-4 position-fixed" style="top: 0;">
+    <!-- Brand Logo -->
+    <a href="" class="brand-link">
+        <img src="./asset/images/logo.png" alt="AdminLTE Logo" class="brand-image img-circle bg-white">
+        <span class="brand-text font-weight-light">SoliCoders</span>
+    </a>
+    <!-- Sidebar -->
+    <div class="sidebar">
+        <!-- Sidebar Menu -->
+        <nav class="mt-2">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                <!-- Skills Management -->
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="fa-solid fa-list-check"></i>
+                        <p>
+                            Gestion des compétences
+                        </p>
+                    </a>
+                </li>
+                <!-- Niveau Management -->
+                <li class="nav-item">
+                    <a href="./index.php" class="nav-link">
+                        <i class="nav-icon fas fa-cog"></i>
+                        <p>
+                            Gestion des niveaux
+                        </p>
+                    </a>
+                </li>
+                <!-- Interns Management -->
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-users"></i>
+                        <p>
+                            Gestion des stagiaires
+                        </p>
+                    </a>
+                </li>
+                
+                <!-- Logout Button -->
+                <li class="nav-item">
+                    <a href="../BLL/authBLO/logoute.php" class="nav-link">
+                        <i class="nav-icon fas fa-sign-out-alt"></i>
+                        <p>
+                            Logout
+                        </p>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+</aside>
